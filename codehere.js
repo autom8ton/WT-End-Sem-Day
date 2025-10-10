f9945c500fe1e002e479a6c723b4860fc01921e5
Lyn101s <lee.lin@computhink.com.sg>
2025-10-10 11:19:03 +0800

    Update codehere.js

--- codehere.js
+++ codehere.js
@@ -1,1 +1,68 @@
+//Clue to 1st digit
+//Part 1:
+//Code to:
+// Session 1: add all numbers divisible by 3 in the range from 1 to 100 inclusive to get the file name
+// Session 2: add all numbers divisible by 7 in the range from 1 to 100 inclusive to get the file name
+
+
+
+
+
+
+
+
+
+
+
+//Part 2: 
+//after getting the answer(filename), write a code to see the display the image (1st digit) by loading the file answer.png to see the 1st digit
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Clue to 2nd digit
+//Part 1: 
+//Code the 1st 10 fibonacci nos and put it as the file name
+//fibonacci is the sum of the 2 preceding nos
+
+
+
+
+
+
+
+
+//Part 2: 
+//after getting the answer(filename), write a code to load the sound file (answer.mp4 to hear the 2nd digit)
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Clue to 3rd digit
+
+//from the array -> s1 for session 1; s2 for session 2, print the character that is a multiple of 6 but the character at index 0 should not be printed
+
+// message_s1 = ['r', 'j', 'a', 't', 'p', 'f', 'e', 'o', 'd', 'v', 'z', 'k', 'i', 'w', 'b', 'y', 'u', 'k', 'g', 'n', 's', 'q', 'c', 'p', 'h', 't', 'm', 'j', 'k', 'h', 't', 'd', 'n','u']; 
+// message_s2 = ['n', 'h', 'p', 'm', 'w', 'a', 's', 'o', 't', 'v', 'q', 'b', 'i', 'l', 'f', 'e', 'k', 'j', 'x', 'd', 'l', 'j']; 
+
+
+
 
